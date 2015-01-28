1294ad677963dcbc55c4183e7cbee61e0b47fade
Daniel Jones <djones8520@gmail.com>
2015-01-27 21:43:27 -0500

    finished code

--- lab3/server/tcpserver.c
+++ lab3/server/tcpserver.c
@@ -27,6 +27,7 @@ int main(int argc, char **argv){
 
 	int len = sizeof(clientaddr);
 
+    char line[5000];
 	while(1){
 		fd_set tmp_set = sockets;
 		select(FD_SETSIZE, &tmp_set, NULL, NULL, NULL);
@@ -35,36 +36,31 @@ int main(int argc, char **argv){
 		for(i=0; i<FD_SETSIZE; i++){
 			if(FD_ISSET(i, &tmp_set)){
 				if(i == sockfd){
-					printf("A client connected\n");
-					int clientsocket = accept(sockfd, (struct sockaddr*) &clientaddr, &len);
+					
+                    char str[INET_ADDRSTRLEN];
+                    inet_ntop( AF_INET, &clientaddr, &str, INET_ADDRSTRLEN );
+                    printf("A client connected (IP=%s : Port=9010)\n",str);
+                    
+                    int clientsocket = accept(sockfd, (struct sockaddr*) &clientaddr, &len);
 					FD_SET(clientsocket, &sockets);
 				}
 				else{
-                    printf("here 1\n");
-                    char line[5000];
-                    //memset(line,'\0',5000);
-                    printf("here 2\n");
-                    recv(i, line, 5000, 0);
-					printf("Got from client: %s\n", line);
-					printf("here 3\n");
-					int j;
-					printf("here 4\n");
-					for(j=4; j<FD_SETSIZE; j++){
-                        if(j != i)
-                            send(j, line, strlen(line), 0);
+                    if(recv(i, line, 5000, 0)) {
+                        printf("Got from client: %s\n", line);
+                        int j;
+                        for(j=4; j<FD_SETSIZE; j++){
+                            if(j != i)
+                                send(j, line, strlen(line), 0);
+                        }
+                        memset(line,0,sizeof(line));
+                    } else {
+                        close(i);
+                        FD_CLR(i, &sockets);
                     }
-					printf("here 5\n");
-					
-					//close(i);
-					//FD_CLR(i, &sockets);
 				}
 			}
 		}
-
-
-		
 	}
 
 	return 0;
 }
-
