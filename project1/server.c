f46b4b9b48e6f05f6675a9f20c3cc20ea93eb3f1
Danny Selgo <selgod@gvsu.edu>
2015-01-28 10:10:17 -0500

    Creating directory and starter file for Project 1.

---
+++ project1/server.c
@@ -0,0 +1,13 @@
+/****************************************************************
+* Project 1: Mini HTTP Server
+* Due 2/4/2015
+*
+* Server File
+*    
+* Michael Kinkema
+* Chase	Pietrangelo
+* Danny	Selgo
+* Daniel Jones
+*
+****************************************************************/
+
