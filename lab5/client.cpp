2b20d917c86a2e95736654ab7fe28068f89d35e0
Daniel Jones <djones8520@gmail.com>
2015-02-14 17:49:41 -0500

    update

--- lab5/client.cpp
+++ lab5/client.cpp
@@ -17,74 +17,78 @@
 using namespace std;
 
 struct dnsheader{
-  u_int16_t id;
-  u_int16_t flags;
-  u_int16_t qcount;
-  u_int16_t ancount;
-  u_int16_t nscount;
-  u_int16_t arcount;
+	u_int16_t id;
+	u_int16_t flags;
+	u_int16_t qcount;
+	u_int16_t ancount;
+	u_int16_t nscount;
+	u_int16_t arcount;
 };
 
 int main(int argc, char** argv){
+	if(argc > 1)
+		string ipaddress = "argv[1]";
+	else
+		string ipaddress = "8.8.8.8";
 
-  int sockfd = socket(AF_INET,SOCK_DGRAM,0);
-  if(sockfd<0){
-    cout<<"There was an error creating the socket"<<endl;
-    return 1;
-  }
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0){
+		cout << "There was an error creating the socket" << endl;
+		return 1;
+	}
 
-  struct sockaddr_in serveraddr;
-  serveraddr.sin_family=AF_INET;
-  serveraddr.sin_port=htons(53);
-  serveraddr.sin_addr.s_addr=inet_addr("8.8.8.8");
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(53);
+	serveraddr.sin_addr.s_addr = inet_addr(ipaddress);
 
-  struct timeval to;
-  to.tv_sec=5;
-  to.tv_usec=0;
-  setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,&to,sizeof(to));
-  
-  cout <<"Enter a domain name: ";
-				
-  string domain;
-  getline(cin,domain);
-  if(domain.back()!='.'){
-    domain+='.';
-  }
+	struct timeval to;
+	to.tv_sec = 5;
+	to.tv_usec = 0;
+	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &to, sizeof(to));
 
-  unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-  default_random_engine generator(seed);
-  uniform_int_distribution<u_int16_t> distribution(0,65535);
+	cout << "Enter a domain name: ";
 
-  char buf[512];
-  dnsheader d;
-  d.id = distribution(generator);
-  u_int16_t flags = 0;
-  flags |= (1<<8);
-  d.flags = htons(flags);
-  d.qcount = htons(1);
-  d.ancount = htons(0);
-  d.nscount = htons(0);
-  d.arcount = htons(0);
+	string domain;
+	getline(cin, domain);
+	if (domain.back() != '.'){
+		domain += '.';
+	}
 
-  memcpy(buf,&d,12);
-  int pos=12;
-  istringstream domainstream(domain);
-  string label;
-  while(getline(domainstream,label,'.').good()){
-    buf[pos++]=label.length();
-    strncpy(&buf[pos],label.c_str(),label.length());
-    pos+=label.length();
-  }
-  buf[pos++]=0;
-  buf[pos++]=0;
-  buf[pos++]=TYPE_A;
-  buf[pos++]=0;
-  buf[pos++]=CLASS_IN;
-  sendto(sockfd,buf,pos,0,
-	 (struct sockaddr*)&serveraddr,sizeof(struct sockaddr_in));
-  cout<<"Sent our query"<<endl;
-  char line[5000];
-  recvfrom(sockfd,line,5000,0,(struct sockaddr*)&serveraddr,(unsigned int*)sizeof(serveraddr));
-  cout << line;
-  return 0;
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_int_distribution<u_int16_t> distribution(0, 65535);
+
+	char buf[512];
+	dnsheader d;
+	d.id = distribution(generator);
+	u_int16_t flags = 0;
+	flags |= (1 << 8);
+	d.flags = htons(flags);
+	d.qcount = htons(1);
+	d.ancount = htons(0);
+	d.nscount = htons(0);
+	d.arcount = htons(0);
+
+	memcpy(buf, &d, 12);
+	int pos = 12;
+	istringstream domainstream(domain);
+	string label;
+	while (getline(domainstream, label, '.').good()){
+		buf[pos++] = label.length();
+		strncpy(&buf[pos], label.c_str(), label.length());
+		pos += label.length();
+	}
+	buf[pos++] = 0;
+	buf[pos++] = 0;
+	buf[pos++] = TYPE_A;
+	buf[pos++] = 0;
+	buf[pos++] = CLASS_IN;
+	sendto(sockfd, buf, pos, 0,
+		(struct sockaddr*)&serveraddr, sizeof(struct sockaddr_in));
+	cout << "Sent our query" << endl;
+	char line[5000];
+	recvfrom(sockfd, line, 5000, 0, (struct sockaddr*)&serveraddr, (unsigned int*)sizeof(serveraddr));
+	cout << line;
+	return 0;
 }
