00e49a1b4a5c5ff00df67633c7fb2bc617d7401d
Danny Selgo <selgod@gvsu.edu>
2015-04-06 21:42:00 -0400

    Packet resend and timeout added

--- project3/server/server.cpp
+++ project3/server/server.cpp
@@ -20,15 +20,22 @@
 #include <iostream>
 #include <pthread.h>
 #include <mutex>
+#include <map>
+#include <utility>
 
 #define BYTES_TO_SEND 256
 #define WINDOW_SIZE 5
 #define BUFLEN 5000
 
+using namespace std;
+
 void strip_newline(char* s);
 void* receiveThread(void* arg);
 
 uint16_t window[WINDOW_SIZE];
+typedef pair<char*,int> dataPair;
+map<int, dataPair> dataMap;
+
 
 // Free window slot
 uint16_t OPEN_SLOT = 65535;
@@ -39,9 +46,8 @@ int sockfd = socket(AF_INET,SOCK_DGRAM,0);
 struct sockaddr_in clientaddr;
 socklen_t slen_client = sizeof(clientaddr);
 
-using namespace std;
-
 std::mutex windowLock;
+std::mutex dataMapLock;
 
 int main(int argc, char **argv)
 {	
@@ -109,8 +115,8 @@ int main(int argc, char **argv)
 			currentSequence++;
 			
 			
-			//if(bytesRead < BYTES_TO_SEND - 3){
-			if(bytesRead > 0){
+			if(bytesRead <= BYTES_TO_SEND - 3 && bytesRead >= 0){
+			//if(bytesRead > 0){
 				if(feof(fp)){
 					header[2] = '1';
 				}else if(ferror(fp)){
@@ -123,6 +129,10 @@ int main(int argc, char **argv)
 			char sendbuff[BYTES_TO_SEND];
 			memcpy(sendbuff,header,3);
 			memcpy(&sendbuff[3],readbuff,bytesRead);
+			
+			dataMapLock.lock();
+			dataMap[currentSequence] = make_pair(sendbuff,bytesRead + 3);
+			dataMapLock.unlock();
 
 			//printf("Server: BytesRead %d\n",bytesRead);
 			//printf("Server: SendBuff Size... %d\n",strlen(sendbuff));
@@ -151,69 +161,101 @@ void* receiveThread(void* arg){
 	char buf[BYTES_TO_SEND];
 
 	cout << "Receive thread created" << endl;
+
+	fd_set select_fds;
+	struct timeval timeout;
+
+	FD_ZERO(&select_fds);
+	FD_SET(sockfd, &select_fds);
+
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
 	
 	while(1){
-		if (recvfrom(sockfd, buf, BYTES_TO_SEND, 0, (struct sockaddr*)&clientaddr, &slen_client) < 0){  
-				printf("Receive error. \n");
-		}
 
-		cout << "ACK" << endl;
-		cout << "buf: " << buf << endl;
-		
-		uint16_t sequenceNumber;
-		memcpy(&sequenceNumber, &buf[0], 2);
-		char dataCheck;
-		memcpy(&dataCheck, &buf[2], 1);
-
-		cout << "dataCheck: " << dataCheck << endl;
-		cout << "sequenceNumber: " << sequenceNumber << endl;
-		
-		// If there is no more data, end the thread
-		if(dataCheck != '0'){
-			cout << "Receive thread exit" << endl;
-			
-			break;
-		}
-
-		int i = 0;
-
-		windowLock.lock();
-		cout << "Thread sequence #: " << sequenceNumber << endl;
-		if(window[i] == sequenceNumber){
-			// window moves
-			window[i] = OPEN_SLOT;
-
-			for(i; i < WINDOW_SIZE; i++){
-				bool found = false;
-				
-				for(int j = i+1; j < WINDOW_SIZE; j++){
-					if(window[j] != OPEN_SLOT && window[j] != ACKNOWLEDGED && !found){
-						window[i] = window[j];
-						window[j] = OPEN_SLOT;
-						
-						found = true;
+		if(select(sockfd, &select_fds, NULL, NULL, &timeout) == 0){
+			cerr << "RESENDING PACKET!" << endl;
+			windowLock.lock();
+			for(int i = 0; i < WINDOW_SIZE; i++){
+				if(window[i] != ACKNOWLEDGED && window[i] != OPEN_SLOT){
+					if(sendto(sockfd,dataMap[window[i]].first,dataMap[window[i]].second,0,(struct sockaddr*)&clientaddr,sizeof(struct sockaddr_in)) < 0){
+						cerr << "Resend Error" << endl;
 					}
+					timeout.tv_sec = 1;
+				}
+			}
+		}else{
+			if (recvfrom(sockfd, buf, BYTES_TO_SEND, 0, (struct sockaddr*)&clientaddr, &slen_client) < 0){  
+				printf("Receive error. \n");
+			}
+
+			cout << "ACK" << endl;
+			cout << "buf: " << buf << endl;
+		
+			uint16_t sequenceNumber;
+			memcpy(&sequenceNumber, &buf[0], 2);
+			char dataCheck;
+			memcpy(&dataCheck, &buf[2], 1);
+
+			cout << "dataCheck: " << dataCheck << endl;
+			cout << "sequenceNumber: " << sequenceNumber << endl;
+		
+			// If there is no more data, end the thread
+			if(dataCheck != '0'){
+				cout << "Receive thread exit" << endl;
 			
-					if(j == (WINDOW_SIZE-1) && !found)
-						window[i] = OPEN_SLOT;
-				}
+				break;
+			}
+
+			int i = 0;
+
+			windowLock.lock();
+			cout << "Thread sequence #: " << sequenceNumber << endl;
+			if( 0 && window[i] == sequenceNumber){
+
+				dataMapLock.lock();
+				dataMap.erase(sequenceNumber);
+				dataMapLock.unlock();
+
+				// window moves
+				window[i] = OPEN_SLOT;
+
+				for(i; i < WINDOW_SIZE; i++){
+					bool found = false;
 				
-				// The last window slot won't be compared and must be set to OPEN_SLOT
-				if(i == (WINDOW_SIZE - 1))
-					window[i] = OPEN_SLOT;
-			}
-		}
-		else{
-			for(int k = 1; k < WINDOW_SIZE; k++){
-				if(window[k] == sequenceNumber){
-					window[k] = ACKNOWLEDGED;
+					for(int j = i+1; j < WINDOW_SIZE; j++){
+						if(window[j] != OPEN_SLOT && window[j] != ACKNOWLEDGED && !found){
+							window[i] = window[j];
+							window[j] = OPEN_SLOT;
+							
+							found = true;
+						}
+			
+						if(j == (WINDOW_SIZE-1) && !found)
+							window[i] = OPEN_SLOT;
+					}
+				
+					// The last window slot won't be compared and must be set to OPEN_SLOT
+					if(i == (WINDOW_SIZE - 1)){
+						window[i] = OPEN_SLOT;
+					}
 				}
 			}
+			else if(0){
+				for(int k = 1; k < WINDOW_SIZE; k++){
+					if(window[k] == sequenceNumber){
+						window[k] = ACKNOWLEDGED;
+						dataMapLock.lock();
+						dataMap.erase(sequenceNumber);
+						dataMapLock.unlock();
+					}
+				}
+			}
+
+			windowLock.unlock();
+
+			memset(buf, 0, sizeof(buf));
 		}
-
-		windowLock.unlock();
-
-		memset(buf, 0, sizeof(buf));
 	}
 }
 
